202273a034053010eabc6387abe0ff31504bbb9f
Saad Malik <36926771+maliksh7@users.noreply.github.com>
2021-08-20 00:42:09 +0500

    Completed!!

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,8 +1,10 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 SIZE = 40
+BACKGROUND_COLOR = (91, 180, 181)
 
 
 class Apple:
@@ -12,9 +14,15 @@ class Apple:
         self.x = SIZE*3
         self.y = SIZE*3
 
+    # drawing apple on screen
     def draw(self):
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
+
+    # regenerating apple else where once snake eats an apple
+    def move(self):
+        self.x = random.randint(0, 24) * SIZE
+        self.y = random.randint(0, 19) * SIZE
 
 
 class Snake:
@@ -26,30 +34,44 @@ class Snake:
         self.direction = 'down'
         self.block = pygame.image.load("resources/block.jpg").convert()
 
+    # drawing snake with blocks
     def draw(self):
-        self.parent_screen.fill((91, 180, 181))
+
         for i in range(self.length):
             self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
+    # increases the length as snake eats an apple
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
+
+    # snake movement towards left
     def move_left(self):
         self.direction = 'left'
 
+    # snake movement towards right
     def move_right(self):
         self.direction = 'right'
 
+    # snake movement towards up
     def move_up(self):
         self.direction = 'up'
 
+    # snake movement towards down
     def move_down(self):
         self.direction = 'down'
 
+    # snake to continue walking unless movement direction is changed by arrows
     def walk(self):
 
+        # update body
         for i in range(self.length-1, 0, -1):
             self.x[i] = self.x[i-1]
             self.y[i] = self.y[i-1]
 
+        # update head
         if self.direction == 'left':
             self.x[0] -= SIZE
         if self.direction == 'right':
@@ -65,19 +87,95 @@ class Snake:
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 600))
+        # pygame.display.set_caption("sh7codes - Snake asnd Apple Game")
+
+        pygame.mixer.init()
+        self.play_bg_music()
+        self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((91, 180, 181))
-        self.snake = Snake(self.surface, 6)
+        self.snake = Snake(self.surface, 1)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    # basic collision fun between two objects
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+
+        return False
+
+    # provide backgroud music for game
+    def play_bg_music(self):
+        pygame.mixer.music.load("resources/bg_music_1.wav")
+        pygame.mixer.music.play()
+
+    # output sound on event occurance
+    def play_sound(self, sound):
+        sound = pygame.mixer.Sound(f"resources/{sound}.wav")
+        pygame.mixer.Sound.play(sound)
+
+    # background picture/ surface for the game
+    def render_bg(self):
+        bg = pygame.image.load("resources/background.jpg")
+        self.surface.blit(bg, (0, 0))
+
+    # @main game Handler/ Controller
     def play(self):
+        self.render_bg()
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
 
+        # Snake eating Apple scenario
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.play_sound("ding")
+            self.snake.increase_length()
+            self.apple.move()
+
+        # Snake colliding its own body
+        for i in range(3, self.snake.length):
+            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                self.play_sound("crash")
+                raise "Collision Occured"
+
+        # snake colliding with the boundries of the window
+        if not (0 <= self.snake.x[0] <= 1000 and 0 <= self.snake.y[0] <= 800):
+            self.play_sound('crash')
+            raise "Hit the boundry error"
+
+    # display score on screen
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(
+            f"Score: {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(score, (800, 10))
+
+    # show a scorecard and replay option once game is over
+    def show_game_over(self):
+        self.render_bg()
+        font = pygame.font.SysFont('arial', 30)
+        line1 = font.render(
+            f"Game is over! Your Score is: {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(line1, (200, 300))
+        line2 = font.render(
+            "To play again press Enter. To exit press Escape!", True, (255, 255, 255))
+        self.surface.blit(line2, (200, 350))
+        pygame.display.flip()
+
+        pygame.mixer.music.pause()
+
+    # reset the game state to initial state as user opt to replay the game
+    def reset(self):
+        self.snake = Snake(self.surface, 1)
+        self.apple = Apple(self.surface)
+
+    # @main Game Logic
     def run(self):
         running = True
+        pause = False
 
         while running:
             for event in pygame.event.get():
@@ -86,26 +184,39 @@ class Game:
                     if event.key == K_ESCAPE:
                         running = False
 
-                    if event.key == K_UP:
-                        self.snake.move_up()
+                    if event.key == K_RETURN:
+                        pygame.mixer.music.unpause()
+                        pause = False
 
-                    if event.key == K_DOWN:
-                        self.snake.move_down()
+                    if not pause:
+                        if event.key == K_UP:
+                            self.snake.move_up()
 
-                    if event.key == K_LEFT:
-                        self.snake.move_left()
+                        if event.key == K_DOWN:
+                            self.snake.move_down()
 
-                    if event.key == K_RIGHT:
-                        self.snake.move_right()
+                        if event.key == K_LEFT:
+                            self.snake.move_left()
+
+                        if event.key == K_RIGHT:
+                            self.snake.move_right()
 
                 elif event.type == QUIT:
                     running = False
 
-            self.play()
+            try:
+                if not pause:
+                    self.play()
+
+            except Exception as e:
+                self.show_game_over()
+                pause = True
+                self.reset()
 
             time.sleep(0.3)
 
 
+# main functioin
 if __name__ == "__main__":
     game = Game()
     game.run()
